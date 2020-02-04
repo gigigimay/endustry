c885db6e6728e5edc9f48e67a2e630682d1ab6d0
gigigimay <mo.maytiya_st@tni.ac.th>
2020-02-04 23:15:31 +0700

    extract searchbar to new file

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/bg_layout.dart';
 import '../widgets/title_text.dart';
+import '../widgets/searchbar.dart';
 import '../constants.dart' as CONSTANT;
 
 class HomePage extends StatelessWidget {
@@ -34,40 +35,3 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class SearchBar extends StatelessWidget {
-  const SearchBar({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100),
-        boxShadow: [CONSTANT.BOX_SHADOW],
-      ),
-      child: RaisedButton(
-        onPressed: () => print('search!!'),
-        elevation: 0,
-        color: Colors.white,
-        splashColor: Colors.purple[50],
-        highlightColor: Colors.purple[50],
-        textColor: Theme.of(context).disabledColor,
-        padding: EdgeInsets.symmetric(vertical: CONSTANT.MD, horizontal: CONSTANT.LG),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-        child: Row(
-          children: <Widget>[
-            Icon(
-              Icons.search,
-              color: Theme.of(context).primaryColor,
-              size: CONSTANT.XX,
-            ),
-            const SizedBox(width: CONSTANT.MD),
-            Text(
-              'ค้นหา',
-              style: TextStyle(fontSize: CONSTANT.FONT_SIZE_HEAD),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}

a657d2bee1e11cabb2aec33c42d3a2436a1ebe63
gigigimay <mo.maytiya_st@tni.ac.th>
2020-02-05 05:25:40 +0700

    update theme

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,15 +8,20 @@ const Color COLOR_BODY = Color(0xff414141);
 
 // spacing size
 /// 4
-const double SIZE_XS = 4.0; 
+const double SIZE_XS = 4.0;
+
 /// 8
 const double SIZE_SM = 8.0;
+
 /// 12
 const double SIZE_MD = 12.0;
+
 /// 16
 const double SIZE_LG = 16.0;
+
 /// 24
 const double SIZE_XL = 24.0;
+
 /// 32
 const double SIZE_XX = 32.0;
 
@@ -26,18 +31,30 @@ const double BORDER_RADIUS = 10.0;
 // font size
 /// 16
 const double FONT_SIZE_BODY = 16.0;
+
 /// 24
 const double FONT_SIZE_HEAD = 24.0;
+
 /// 36
 const double FONT_SIZE_TITLE = 36.0;
 
 // font style
+const TextStyle TEXT_STYLE_BODY =
+    TextStyle(fontSize: FONT_SIZE_BODY, color: COLOR_BODY);
 const TextStyle TEXT_STYLE_BODY_PRIMARY = TextStyle(
-    color: COLOR_PRIMARY);
+    color: COLOR_PRIMARY,
+    fontSize: FONT_SIZE_BODY,
+    fontWeight: FontWeight.w700);
 const TextStyle TEXT_STYLE_HEADING = TextStyle(
     fontSize: FONT_SIZE_HEAD, color: COLOR_BODY, fontWeight: FontWeight.w700);
 const TextStyle TEXT_STYLE_HEADING_PRIMARY = TextStyle(
-    fontSize: FONT_SIZE_HEAD, color: COLOR_PRIMARY, fontWeight: FontWeight.w700);
+    fontSize: FONT_SIZE_HEAD,
+    color: COLOR_PRIMARY,
+    fontWeight: FontWeight.w700);
+const TextStyle TEXT_STYLE_TITLE = TextStyle(
+    fontSize: FONT_SIZE_TITLE,
+    fontWeight: FontWeight.w700,
+    color: COLOR_PRIMARY);
 
 // boxShadow
 const BoxShadow BOX_SHADOW = BoxShadow(

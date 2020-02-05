2b48c731220fe305bd2a4cbdc6ee9eab0da3e113
gigigimay <mo.maytiya_st@tni.ac.th>
2020-02-05 16:27:53 +0700

    fix home layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,47 +7,56 @@ class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
         body: BgLayout(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              PagePadding(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
+          child: Stack(children: <Widget>[
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  PagePadding(
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        TitleText('กระทรวง'),
-                        TitleText('อุตสาหกรรม', fontSize: 50.0, height: 0.8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            TitleText('กระทรวง'),
+                            TitleText('อุตสาหกรรม',
+                                fontSize: 50.0, height: 0.8),
+                          ],
+                        ),
+                        const SizedBox(height: CONSTANT.SIZE_XX),
+                        SearchBar(),
                       ],
                     ),
-                    const SizedBox(height: CONSTANT.SIZE_XX),
-                    SearchBar(),
-                  ],
-                ),
-              ),
-              const SizedBox(height: CONSTANT.SIZE_XL),
-              Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: <Widget>[
-                      ListGroup(title: 'การใช้งานล่าสุด', items: MOCK_SERVICES),
-                      ListGroup(title: 'บริการแนะนำ', items: MOCK_SERVICES),
-                      ListGroup(title: 'คลังความรู้แนะนำ', items: MOCK_SERVICES),
-                    ],
                   ),
-                ),
+                  const SizedBox(height: CONSTANT.SIZE_XL),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: EdgeInsets.only(bottom: height * 0.1),
+                      scrollDirection: Axis.vertical,
+                      child: Column(
+                        children: <Widget>[
+                          ListGroup(
+                              title: 'การใช้งานล่าสุด', items: MOCK_SERVICES),
+                          ListGroup(title: 'บริการแนะนำ', items: MOCK_SERVICES),
+                          ListGroup(
+                              title: 'คลังความรู้แนะนำ', items: MOCK_SERVICES),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              NavigationBar(
-                currentpage: 'home',
-              )
-            ],
-          ),
+            ),
+            NavigationBar(
+              currentpage: 'home',
+            )
+          ]),
         ),
       ),
     );

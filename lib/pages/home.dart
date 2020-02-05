ba54f879c8822831cc96e4a647fd6e4719190570
gigigimay <mo.maytiya_st@tni.ac.th>
2020-02-05 22:38:38 +0700

    format code

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,32 +10,31 @@ class HomePage extends StatelessWidget {
     return BgLayout(
       navbar: NavigationBar(currentpage: 'home'),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          PagePadding(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                PagePadding(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      TitleText('กระทรวง'),
-                      TitleText('อุตสาหกรรม', fontSize: 50.0, height: 0.8),
-                      const SizedBox(height: CONSTANT.SIZE_XX),
-                      SearchBar(),
-                      const SizedBox(height: CONSTANT.SIZE_MD),
-                    ],
-                  ),
-                ),
-          PageScrollBody(
-                    child: Column(
-                      children: <Widget>[
-                ListGroup(title: 'การใช้งานล่าสุด', items: MOCK_SERVICES),
-                        ListGroup(title: 'บริการแนะนำ', items: MOCK_SERVICES),
-                        ListGroup(
-                            title: 'คลังความรู้แนะนำ', items: MOCK_SERVICES),
-                      ],
-                    ),
-                  ),
+                TitleText('กระทรวง'),
+                TitleText('อุตสาหกรรม', fontSize: 50.0, height: 0.8),
+                const SizedBox(height: CONSTANT.SIZE_XX),
+                SearchBar(),
+                const SizedBox(height: CONSTANT.SIZE_MD),
               ],
             ),
+          ),
+          PageScrollBody(
+            child: Column(
+              children: <Widget>[
+                ListGroup(title: 'การใช้งานล่าสุด', items: MOCK_SERVICES),
+                ListGroup(title: 'บริการแนะนำ', items: MOCK_SERVICES),
+                ListGroup(title: 'คลังความรู้แนะนำ', items: MOCK_SERVICES),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

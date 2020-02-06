69bc1ada74d42e3c240399806b2d0a33148585ac
ne <naesup@gmail.com>
2020-02-07 01:58:18 +0700

    add knowledge item
    for rebase

--- lib/pages/knowledge.dart
+++ lib/pages/knowledge.dart
@@ -1,15 +1,45 @@
 import 'package:endustry/export.dart';
 import 'package:endustry/constants.dart' as CONSTANT;
+import 'package:endustry/widgets/home/list_group.dart';
+import 'package:endustry/widgets/knowledge.dart/knowledge_item.dart';
 
 class KnowledgePage extends StatelessWidget {
   const KnowledgePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return BgLayout(
       navbar: NavigationBar(currentpage: 'knowledge'),
-      child: Column(
-        children: <Widget>[PagePadding(child: TitleText('Knowledge Page!'))],
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ListGroup(
+              title: 'คลังความรู้แนะนำ',
+              items: [],
+            ),
+            PagePadding(
+                child: Column(
+              children: [
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1'
+              ].map((item) => KnowledgeItem()).toList(),
+            ))
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/knowledge.dart/knowledge_item.dart
@@ -0,0 +1,26 @@
+import 'package:endustry/export.dart';
+import 'package:endustry/constants.dart' as CONSTANT;
+import 'package:endustry/widgets/home/list_group.dart';
+
+class KnowledgeItem extends StatelessWidget {
+  const KnowledgeItem({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[Text('data'), Icon(Icons.star_border)],
+          ),
+          Container(
+            child: Text('data2'),
+          )
+        ],
+      ),
+    );
+  }
+}

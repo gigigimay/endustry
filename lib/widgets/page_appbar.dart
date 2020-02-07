a25273e11bd8b02952b255c7e7b00f5edc1f69d3
ne <naesup@gmail.com>
2020-02-07 17:02:55 +0700

    fix padding

--- lib/widgets/page_appbar.dart
+++ lib/widgets/page_appbar.dart
@@ -22,28 +22,35 @@ class PageAppBar extends StatefulWidget {
 class _PageAppBarState extends State<PageAppBar> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            widget.haveBackArrow
-                ? GestureDetector(
-                    onTap: widget.backArrowFunction,
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      color: CONSTANT.COLOR_PRIMARY,
-                      size: CONSTANT.SIZE_XX,
-                    ))
-                : Container(),
-            SizedBox(
-              width: CONSTANT.SIZE_MD,
-            ),
-            TitleText(widget.title)
-          ],
-        ),
-        widget.actionWidget
-      ],
+    return PagePadding(
+      top: 0.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              widget.haveBackArrow
+                  ? Row(
+                      children: <Widget>[
+                        GestureDetector(
+                            onTap: widget.backArrowFunction,
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: CONSTANT.COLOR_PRIMARY,
+                              size: CONSTANT.SIZE_XX,
+                            )),
+                        SizedBox(
+                          width: CONSTANT.SIZE_MD,
+                        ),
+                      ],
+                    )
+                  : Container(),
+              TitleText(widget.title)
+            ],
+          ),
+          widget.actionWidget ?? Container()
+        ],
+      ),
     );
   }
 }

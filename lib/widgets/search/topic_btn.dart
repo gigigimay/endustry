f0cc391479a8c06e25691bf08e0eae2bf40b6eaf
gigigimay <mo.maytiya_st@tni.ac.th>
2020-02-12 21:41:34 +0700

    refactor topic menu of search page

--- lib/widgets/search/topic_btn.dart
+++ lib/widgets/search/topic_btn.dart
@@ -2,18 +2,34 @@ import 'package:endustry/export.dart';
 import 'package:endustry/constants.dart' as CONSTANT;
 
 class TopicBtn extends StatelessWidget {
-  const TopicBtn({Key key, this.text, this.style, this.onPressed})
-      : super(key: key);
-  final text;
-  final style;
-  final VoidCallback onPressed;
+  const TopicBtn({
+    Key key,
+    this.text,
+    this.isActive,
+    this.onChange,
+  }) : super(key: key);
+
+  final String text;
+  final bool isActive;
+  final Function onChange;
+
+  void onPressed() {
+    onChange(this.text);
+  }
 
   @override
   Widget build(BuildContext context) {
     return FlatButton(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(CONSTANT.BORDER_RADIUS)),
-        child: Text(text, style: style),
-        onPressed: onPressed);
+      onPressed: onPressed,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(CONSTANT.BORDER_RADIUS),
+      ),
+      child: Text(
+        text,
+        style: isActive
+            ? CONSTANT.TEXT_STYLE_BODY_PRIMARY
+            : TextStyle(fontWeight: FontWeight.w300),
+      ),
+    );
   }
 }

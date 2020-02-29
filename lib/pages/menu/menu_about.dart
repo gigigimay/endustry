01d69ce8d02b69170a058180429ad31b39b29d67
gigigimay <mo.maytiya_st@tni.ac.th>
2020-03-01 06:52:01 +0700

    update menu about page ui

--- lib/pages/menu/menu_about.dart
+++ lib/pages/menu/menu_about.dart
@@ -1,10 +1,12 @@
 import 'package:endustry/export.dart';
+import 'package:endustry/constants.dart' as CONSTANT;
 
 class MenuAboutPage extends StatelessWidget {
   const MenuAboutPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return BgLayout(
       child: Column(
         children: <Widget>[
@@ -14,6 +16,32 @@ class MenuAboutPage extends StatelessWidget {
             backArrowFunction: () {
               Navigator.pop(context);
             },
+          ),
+          PagePadding(
+            child: RoundedBox(
+              padding: EdgeInsets.symmetric(
+                horizontal: CONSTANT.SIZE_XL,
+                vertical: CONSTANT.SIZE_XX,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      width: width * 0.4,
+                      child: Image(image: AssetImage('assets/images/LOGO.png')),
+                      padding: EdgeInsets.only(bottom: CONSTANT.SIZE_XX),
+                    ),
+                  ),
+                  Text(
+                    'ข้อมูลเกี่ยวกับกระทรวงอุตสาหกรรม',
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                      'กระทรวงอุตสาหกรรม (อังกฤษ: Ministry of Industry) เป็นส่วนราชการของรัฐบาลไทยประเภทกระทรวง มีอำนาจหน้าที่เกี่ยวกับการส่งเสริมและพัฒนาอุตสาหกรรม การพัฒนาผู้ประกอบการ และราชการอื่นตามที่มีกฎหมายกำหนดให้เป็นอำนาจหน้าที่ของกระทรวงอุตสาหกรรม หรือส่วนราชการที่สังกัดกระทรวงอุตสาหกรรม'),
+                ],
+              ),
+            ),
           )
         ],
       ),
